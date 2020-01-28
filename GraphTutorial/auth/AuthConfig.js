643cdc3b0bf0e76ca12d8311f833f804f22a86c8
Renato Bassi <rena.futbol@gmail.com>
2020-01-28 13:59:16 -0300

    Login MS Graph

---
+++ GraphTutorial/auth/AuthConfig.js
@@ -0,0 +1,10 @@
+export const AuthConfig = {
+  appId: '37e2278f-26ca-425c-8bea-a104bf513e3d',
+  appScopes: [
+    'openid',
+    'offline_access',
+    'profile',
+    'User.Read',
+    'Calendars.Read'
+  ]
+};
---
+++ graph-tutorial/src/Config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  appId: '7b50e385-6c0d-44d9-9abb-851404575c2e',
+  redirectUri: 'http://localhost:3000',
+  scopes: [
+    'user.read',
+    'calendars.read'
+  ]
+};
